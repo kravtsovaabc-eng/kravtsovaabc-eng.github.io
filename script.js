4ad0c6bd90fdf27073bb84636ed3f082fcd6c6d4
kravtsovaabc-eng <kravtsova.abc@gmail.com>
2026-01-29 21:00:52 +0100

    Refactor button click listener to use arrow function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+"use strict";
+
 const button = document.getElementById("startBreathing");
 const text = document.getElementById("breathingText");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
   text.textContent = "Inhale… 2… 3… 4… Hold… 2… 3… 4… Exhale… 2… 3… 4…";
 });
